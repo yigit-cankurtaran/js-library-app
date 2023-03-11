66bb68de418742b36874eabe35899addd163cc55
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-11 16:04:33 +0300

    Created the input form for the book, made it invisible.

---
+++ app.js
@@ -0,0 +1,18 @@
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render();
+}
