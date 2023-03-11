e73d232438745307c52eb1c117d79ea9a435768c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-11 14:14:00 +0300

    Made the button add our element. Now I'll change the HTML and CSS around a bit.

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const button = document.getElementById("newBut");
-button.addEventListener("click", function () {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("newDiv");
-  newDiv.innerHTML = "Hello World";
-  document.body.appendChild(newDiv);
+const addButton = document.getElementById("addButton");
+addButton.addEventListener("click", function () {
+  // create a new div element in the box class, with the read and pages id divs saying hello and world
+
+  const newBook = document.createElement("div");
+  newBook.classList.add("box");
+  newBook.innerHTML = `
+    <div id="read">Hello</div>
+    <div id="pages">World</div>
+    `;
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("bookList");
+  document.body.insertBefore(newBook, currentDiv);
 });
