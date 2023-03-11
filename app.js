42177f524c67e18e5402323b98f0073431cc762f
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-11 15:00:18 +0300

    Too much to work with. Will need to restructure the code to keep it simple and easy to maintain.

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const addButton = document.getElementById("addButton");
-addButton.addEventListener("click", function () {
-  // create a new div element in the box class, with the read and pages attributes saying hello and world
-  const newBook = document.createElement("div");
-  newBook.classList.add("box");
-  newBook.innerHTML = `
-        <div id="read">
-          <h3>Read Status</h3>
-        </div>
-        <div id="pages">
-          <h3>How many pages the book is</h3>
-        </div>
-        <div id="authors">
-          <h3>Who is the author?</h3>
-        </div>
-`;
-  // add the text node to the newly created div
-  newBook.appendChild(document.createTextNode("New Book"));
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("bookList");
-  document.body.insertBefore(newBook, currentDiv);
-});
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+let myLibrary = [];
+let book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read");
+
+const addButton = document.querySelector("#addButton");
+addButton.addEventListener("click", addBookToLibrary);
